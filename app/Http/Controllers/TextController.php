a2da35e5a9ff8e2ec6ebf43177f5ce5fd4ab1d33
ale-lit <zeldor@ya.ru>
2022-08-13 03:17:13 +0300

    feat: add new text functional

---
+++ app/Http/Controllers/TextController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Text;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class TextController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('main');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => ['required', 'string'],
+            'text' => ['required', 'string'],
+            'author' => ['nullable', 'string', 'max:50'],
+            'email' => ['nullable', 'email'],
+        ]);
+        $validated['title'] = Str::limit($validated['title'], 252, '...');
+
+        $text = new Text;
+        $text->ip = request()->ip();
+
+        // Generate slug
+        $preSlug = Str::slug(Str::limit($validated['title'], 50), '-');
+        $slug = $preSlug . today()->format('-d-m-y');
+        for ($i = 1; Text::firstWhere('slug', $slug); $i++) {
+            $slug = $preSlug . '-' . $i . today()->format('-d-m-y');
+        }
+        $text->slug = $slug;
+
+        $result = $text->fill($validated)->save();
+
+        if ($result) {
+            // notice('Пост успешно опубликован!', 'success');
+            // ! TODO: Отправить email $validated['email']
+            return redirect()
+                ->route('text.show', $slug);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInput();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_08_12_061124_create_texts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('texts', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip', 45);
+            $table->string('slug', 65);
+            $table->string('title', 255);
+            $table->text('text');
+            $table->string('author', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('texts');
+    }
+};
