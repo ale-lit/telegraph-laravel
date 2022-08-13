5573c035afcce4a197de38e22b5abe9f5eb91449
ale-lit <zeldor@ya.ru>
2022-08-13 04:50:57 +0300

    feat: add check access helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,18 @@
 <?php
 
-if (!function_exists('notice')) {
-    function notice(string $message, string $type)
+use Illuminate\Support\Facades\Hash;
+
+if (! function_exists('notice')) {
+    function notice(string $message, string $type): void
     {
         $sessionName = 'notice' . $type;
         session([$sessionName => $message]);
+    }
+}
+
+if (! function_exists('checkAccess')) {
+    function checkAccess(string $slug, string $token): bool
+    {
+        return Hash::check(config('app.key') . $slug, $token);
     }
 }
